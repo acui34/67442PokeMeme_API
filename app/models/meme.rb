2e698e97226ce28ebd031dce598664bb1c93c808
Chelsea Cui <acui@andrew.cmu.edu>
2019-11-05 15:37:23 -0500

    fix posttime

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -5,7 +5,7 @@ class Meme < ApplicationRecord
   belongs_to :user
 
   # validation
-  validates_presence_of :station_id, :user_id, :post_time, :image_url
+  validates_presence_of :station_id, :user_id, :image_url
 
   # scopes
   scope :sort_by_post_time_asc, -> (station_id) { where(station_id: station_id).order(:post_time) }
