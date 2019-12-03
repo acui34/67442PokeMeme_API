8c0a69648b5660785ff0b16dc91d42f30db9c684
Chelsea Cui <acui@andrew.cmu.edu>
2019-12-03 15:20:51 -0500

    update api

--- app/serializers/meme_serializer.rb
+++ app/serializers/meme_serializer.rb
@@ -1,5 +1,5 @@
 class MemeSerializer < ActiveModel::Serializer
-  attributes :user_name, :station, :image_url, :post_time, :e1_like, :e2_like, :e3_like, :e4_like
+  attributes :id, :user_name, :station, :image_url, :post_time, :e1_like, :e2_like, :e3_like, :e4_like
 
   def user_name
     object.user.name
