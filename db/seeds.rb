b84b67d265e4812837b5c821a88c8eaa3770003c
Chelsea Cui <acui@andrew.cmu.edu>
2019-12-05 09:48:23 -0500

    remove problematic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,3 @@ chelsea = User.create(first_name: "Chelsea", last_name: "Cui", fb_access_token: 
 inoodle = Station.create(name: "iNoodle", longitude: -79.9455855, latitude: 40.4433813, introduction: "iNoodle is a really really really really really really really really really really really really really really cool place.")    
 underground = Station.create(name: "Underground", longitude: -79.9431901, latitude: 40.4454289, introduction: "underground is super super awesome.")    
 hunt = Station.create(name: "Hunt", longitude: -79.943736, latitude: 40.4411172, introduction: "Hunt is the evil place.")    
-
-#TODO: change image url after connect to Heroku
-meme1 = Meme.create(station: inoodle, user: roxanne, post_time: DateTime.now.beginning_of_day, image_url: "www.meme1.com")
-meme2 = Meme.create(station: inoodle, user: chelsea, post_time: DateTime.now.beginning_of_day-10, image_url: "www.meme2.com", e2_like: 1, e1_like: 1)     
-meme3 = Meme.create(station: inoodle, user: cindy, post_time: DateTime.now.beginning_of_day-20, image_url: "www.meme3.com", e1_like: 3, e2_like: 5)    
-meme4 = Meme.create(station: hunt, user: chelsea, post_time: DateTime.now.beginning_of_day, image_url: "www.meme4.com")    
