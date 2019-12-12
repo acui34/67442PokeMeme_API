2b8297e48e8b19e2f018be72b83490a13d35b76b
Chelsea Cui <acui@andrew.cmu.edu>
2019-12-11 22:35:20 -0500

    add meme poster photo

--- app/serializers/meme_serializer.rb
+++ app/serializers/meme_serializer.rb
@@ -1,8 +1,12 @@
 class MemeSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :user_name, :station, :image_url, :post_time, :e1_like, :e2_like, :e3_like, :e4_like
+  attributes :id, :user_id, :user_photo, :user_name, :station, :image_url, :post_time, :e1_like, :e2_like, :e3_like, :e4_like
 
   def user_id
     object.user.id
+  end
+
+  def user_photo
+    object.user.photo
   end
 
   def user_name
